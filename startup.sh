b84536e7a0f73841c593c8877bb9848cf5c8cb0f
oprogramador <uomodislesia@gmail.com>
2014-12-01 11:08:09 +0100

    startup bug fix

--- startup.sh
+++ startup.sh
@@ -1,14 +1,20 @@
-sudo apt-get update && 
-sudo apt-get install realpath curl apache2 php5 php5-cli mysql-client mysql-server php5-mysql php5-gd && 
-mysql -u root -p < createdb.sql && 
-mysql -u root -p chat < db.sql && 
-cd ~ &&
-if [ ! -f composer.phar ]; then 
-    curl -sS https://getcomposer.org/installer | php
-fi && 
-cd - &&
+#sudo apt-get update && 
+#sudo apt-get install realpath curl apache2 php5 php5-cli mysql-client mysql-server php5-mysql php5-gd && 
+#mysql -u root -p < createdb.sql && 
+#mysql -u root -p chat < db.sql && 
+#cd ~ &&
+#if [ ! -f composer.phar ]; then 
+    #curl -sS https://getcomposer.org/installer | php
+#fi && 
+#cd - &&
+sudo chmod -R 777 .
+curdir=`pwd` &&
 php ~/composer.phar update && 
-sudo chmod -R 777 . &&
+while [ `pwd` != '/' ]; do
+    sudo chmod 777 . &&
+    cd ..
+done &&
+cd $curdir &&
 sudo mkdir -p /var/www/html/cgi/ && 
 if [ ! -L /var/www/html/cgi/ajax_chat ]; then
     sudo ln -s `realpath .` /var/www/html/cgi/ajax_chat
