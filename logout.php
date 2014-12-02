69ed316f7386e8609c9d05faadc205420ce09e60
Your Name <a@a.com>
2014-12-02 10:38:02 +0100

    logout bug fix

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 require_once 'util.php';
 
 $login = Util::getLogin();
-Util::query("DELETE FROM users WHERE name='%s' and (password='' or password is NULL)", $login);
+Util::query("DELETE FROM users WHERE name='%s' and (password='' or password is NULL)", [$login]);
 Util::clearSession();
 
 header("Location: view1.php");
